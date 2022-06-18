59e64252c7f7563d92de269bd52584347817f381
berkealgul <berkealgul@gmail.com>
2022-06-18 08:34:20 +0300

    wip2

--- src/cropper.py
+++ src/cropper.py
@@ -12,25 +12,26 @@ class ROISelector:
         cv2.namedWindow('ROI Selection')  
         cv2.setMouseCallback('ROI Selection', self.roiMouseHandler)  
 
+        while self.roiSelected is False:
+            cv2.waitKey(0)
+
     def roiMouseHandler(self, event, x, y, flags, param):  
         self.activeX = x
         self.activeY = y
+
+        frame_ = self.frame.copy()
+
+        v0 = (self.roi_x, self.roi_y)
+        v1 = (self.activeX, self.activeY)
+        cv2.rectangle(frame_, v0, v1, (0,255,0), 2)
+        cv2.imshow('ROI Selection', frame_)
+        #cv2.waitKey(0)
 
         if(event == cv2.EVENT_LBUTTONDBLCLK):  
             print("click")
         elif(event == cv2.EVENT_RBUTTONDBLCLK):  
             pass
 
-    def roiSelection(self, frame):
-
-
-        while self.roiSelected is False:
-            v0 = (self.roi_x, self.roi_y)
-            v1 = (self.activeX, self.activeY)
-            print(v1)
-            cv2.rectangle(frame, v0, v1, (0,255,0), 2)
-            cv2.imshow('ROI Selection', frame)
-            cv2.waitKey(0)
 
 
 def main():
@@ -51,8 +52,7 @@ def main():
     n = 0
 
     ret, frame = cap.read()
-    rs = ROISelector()
-    rs.roiSelection(frame)
+    rs = ROISelector(frame)
 
     while True:
         ret, frame = cap.read()
