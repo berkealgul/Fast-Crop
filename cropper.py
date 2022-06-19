d7718c5704e491312f6cbf52a9ef9596225bea9a
berkealgul <berkealgul@gmail.com>
2022-06-19 04:07:19 +0300

    frame selection done

--- cropper.py
+++ cropper.py
@@ -1,5 +1,6 @@
 import cv2
 
+WINDOW_WAIT_TIME = 20 # ms
 
 class RoiManager:
     def __init__(self):
@@ -11,7 +12,6 @@ class RoiManager:
         self.roiH = -1
         self.roiSelected = False
         self.frame = None
-        self.windowWaitTime = 20 # ms
 
     def setRoi(self, frame):
         self.frame = frame
@@ -24,7 +24,7 @@ class RoiManager:
         cv2.namedWindow(winName)  
         cv2.setMouseCallback(winName, self.roiMouseHandler)  
         while self.roiSelected is False:
-            cv2.waitKey(self.windowWaitTime)
+            cv2.waitKey(WINDOW_WAIT_TIME)
         cv2.destroyWindow(winName)
 
     def roiMouseHandler(self, event, x, y, flags, param):  
@@ -48,7 +48,7 @@ class RoiManager:
         cv2.imshow(winName, roi)
 
         while True:
-            key = cv2.waitKey(self.windowWaitTime)
+            key = cv2.waitKey(WINDOW_WAIT_TIME)
             if key == 81 or key == 113:
                 break # accept roi
             elif key == 69 or key == 101:
@@ -73,25 +73,63 @@ class RoiManager:
             self.roiW = self.activeX - self.roiX
             self.roiH = self.activeY - self.roiY
             self.roiSelected = True
+
+
+class TimeLapseManager:
+    def __init__(self, cap):
+        self.cap = cap
+        self.videoLength =  int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.videoBeginPos = -1
+        self.winName = "TimeLapseManager"
+        _ , self.videoBeginFrame = cap.read()
+
+
+    def handleFrameSelectionSlider(self, value):
+        # slide values are % based hence we gotta calculate the position
+        self.videoBeginPos = int(self.videoLength * min((value/100), 1.0))
+        self.cap.set(1, self.videoBeginPos)
+        _, self.videoBeginFrame = self.cap.read()
+        cv2.imshow(self.winName, self.videoBeginFrame)
+
+
+    # this function makes user selects any frame from video and returns selected frame
+    def selectFrameFromVideo(self):
+        cv2.namedWindow(self.winName)
+        cv2.createTrackbar("Video Position %", self.winName, 0, 100, self.handleFrameSelectionSlider)
+        self.handleFrameSelectionSlider(0)
+
+        while True: # space to complete
+            key = cv2.waitKey(WINDOW_WAIT_TIME)
+            if key == 81 or key == 113:
+                break
             
-
-
+        cv2.destroyWindow(self.winName)
+        return self.videoBeginFrame # return the resulting frame
+    
 
 def main():
     cap = cv2.VideoCapture("example.mp4")
     fps = cap.get(cv2.CAP_PROP_FPS)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    writer = cv2.VideoWriter('outpy.mp4', fourcc, fps, (w,h)) 
+    #writer = cv2.VideoWriter('outpy.mp4', fourcc, fps, (w,h)) 
 
 
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     n = 0
 
     ret, frame = cap.read()
-    rs = RoiManager()
-    rs.setRoi(frame)
+    frame = TimeLapseManager(cap).selectFrameFromVideo()
+    #rs = RoiManager()
+    #rs.setRoi(frame)
+
+    cv2.imshow("new frame", frame)
+
+    # it will be changed
+    while True:
+        cv2.waitKey(100)
 
     while True:
+
         ret, frame = cap.read()
         
         n+=1
@@ -105,6 +143,7 @@ def main():
 
     writer.release()
     cap.release()
+
 
 if __name__ == "__main__":
     main()
